d66d43cd8960453fdbeb4fe4905bd17d3e0a755b
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2009-03-03 09:27:58 +0000

    run tests on lxplus

--- test/traintreeSaver_cfg.py
+++ test/traintreeSaver_cfg.py
@@ -1,31 +1,30 @@
 import FWCore.ParameterSet.Config as cms
 
 #-------------------------------------------------
-# test cfg file for mva training for jet parton 
-# association
+# test cfg file for mva training for event
+# selection
 #-------------------------------------------------
 process = cms.Process("TEST")
 
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.threshold = 'WARNINGS'
+process.MessageLogger.cerr.threshold = 'INFO'
 
 #-------------------------------------------------
 # process configuration
 #-------------------------------------------------
 
-## define your trainingfile(s)
+## define input
 process.source = cms.Source("PoolSource",
-     fileNames = cms.untracked.vstring(
-       #please put your input files here
-       #example: "file:/tmp/mrenz/CMSSW_2_2_4/src/job_1_TQAFLayer1_Output.root",
+    fileNames = cms.untracked.vstring(
+    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
      ),
      skipEvents = cms.untracked.uint32(0)
 )
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(100)
 )
 
 ## configure process options
@@ -43,15 +42,16 @@ process.GlobalTag.globaltag = cms.string('IDEAL_V9::All')
 ## load magnetic field
 process.load("Configuration.StandardSequences.MagneticField_cff")
 
-
 #-------------------------------------------------
 # tqaf configuration
 #-------------------------------------------------
 
 ## std sequence for tqaf layer1
-#process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
 #process.load("PhysicsTools.HepMCCandAlgos.genEventProcID_cfi")
 
+## std sequence for ttGenEvent
+process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 
 ## configure mva trainer
 process.load("TopQuarkAnalysis.TopEventSelection.TraintreeSaver_cff")
@@ -60,13 +60,19 @@ process.load("TopQuarkAnalysis.TopEventSelection.TraintreeSaver_cff")
 from TopQuarkAnalysis.TopEventSelection.TraintreeSaver_cff import looper
 process.looper = looper
 
+## necessary fixes to run 2.2.X on 2.1.X data
+## comment this when running on samples produced with 22X
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
+run22XonSummer08AODSIM(process)
+
 #-------------------------------------------------
-# process paths;
+# process paths
 #-------------------------------------------------
 
-## run tqafLayer1
+## produce tqafLayer1 and and ttGenEvt
 #process.p0 = cms.Path(process.genEventProcID * process.tqafLayer1)
-                      
+process.p0 = cms.Path(process.tqafLayer1 *
+                      process.makeGenEvt)
 
 ## make mva training
 process.p1 = cms.Path(process.saveTrainTree)
